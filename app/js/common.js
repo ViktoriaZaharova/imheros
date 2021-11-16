830d1d93e5bc05764f24f01d8a2b3ee6d03ff46a
Viktoria <zaharova181114@gmail.com>
2021-11-17 00:44:45 +0200

    add pages

--- app/js/common.js
+++ app/js/common.js
@@ -21,3 +21,43 @@ $('.btn-burger').on('click', function () {
 $('.btn-close').on('click', function () {
     $('.mobile-menu').fadeOut();
 });
+
+$('.btn-toggle-plus').on('click', function () {
+   $(this).toggleClass('click').parents('.solider-box').find('.solider-box__body').slideToggle();
+});
+
+$('.dropdown-toggle').on('click', function (e) {
+    e.preventDefault();
+    $(this).find('.dropdown-toggle__icon').fadeToggle();
+    $(this).siblings('.dropdown-menu').slideToggle();
+});
+
+$('.gallery-slider').slick({
+    slidesToShow: 4,
+    arrows: false,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 2,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 1,
+                arrows: true
+            }
+        }
+    ]
+});
